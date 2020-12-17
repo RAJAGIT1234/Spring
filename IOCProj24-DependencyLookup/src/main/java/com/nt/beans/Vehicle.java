d8d5c520c09775b4c0de132033b73c4cf1f54dd7
RAJA <RAJA@Raja>
2020-12-17 11:38:14 -0500

    mango db

---
+++ IOCProj24-DependencyLookup/src/main/java/com/nt/beans/Vehicle.java
@@ -0,0 +1,41 @@
+package com.nt.beans;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+public class Vehicle {
+	
+	
+	public  void  entertainment() {
+		System.out.println("Vehicle is equipped with  DVD Player  for entertainment");
+	}
+	
+	public  void soundHorn() {
+		System.out.println("Vehicle  is equippled with skoda horn  ");
+	}
+	
+	public  void  fillFuel() {
+		System.out.println("Vehicle is having fuel tank of 50 liters");
+	}
+	
+	public void  journey(String  sourcePlace , String destPlace) {
+		System.out.println("Vehicle.journey()");
+		DefaultListableBeanFactory factory=null;
+		XmlBeanDefinitionReader reader=null;
+		Engine engg=null;
+		//create an extra IOC Container 
+		factory=new DefaultListableBeanFactory();
+		reader=new XmlBeanDefinitionReader(factory);
+		reader.loadBeanDefinitions("com/nt/cfgs/applicationContext.xml");
+		System.out.println("Extra container");
+		//get Dependent  Bean class object  (Dependency lookup)
+		engg=factory.getBean("engg", Engine.class);
+		engg.start();
+		System.out.println("journey  started  at ::"+sourcePlace);
+		System.out.println("journey is going on.......  from "+sourcePlace+" to "+destPlace);
+		engg.stop();
+		System.out.println("jounery ended at ::"+destPlace);
+	}
+	
+
+}
