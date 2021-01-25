77999d349e4f702cd58932599b8d7da449c40b6a
RAJA <RAJA@Raja>
2021-01-25 18:56:14 -0500

    setterinjection

---
+++ IOCProj3-CyclicDl-setterInjection/src/com/nt/beans/A.java
@@ -0,0 +1,20 @@
+package com.nt.beans;
+
+public class A {
+
+	private B b;
+
+	public A() {
+		System.out.println("A.O-param constructor");
+	}
+	public void setB(B b) {
+		System.out.println("A.setB()");
+		this.b = b;
+	}
+
+	@Override
+	public String toString() {
+		return "A [b=" ;
+	}
+	
+}
