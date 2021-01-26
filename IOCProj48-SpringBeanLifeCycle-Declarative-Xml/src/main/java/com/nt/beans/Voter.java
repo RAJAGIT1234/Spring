e0ef55b7cd202298ec34d10377944f8417cd57f1
RAJA <RAJA@Raja>
2021-01-25 19:09:36 -0500

    SpringBeanLifecycle

---
+++ IOCProj48-SpringBeanLifeCycle-Declarative-Xml/src/main/java/com/nt/beans/Voter.java
@@ -0,0 +1,67 @@
+package com.nt.beans;
+
+import java.util.Date;
+
+public class Voter {
+   private String name;
+   private float age;
+   private Date dov;
+  
+   
+   public Voter() {
+	   System.out.println("Voter::0-Param Consstructor");
+   }
+
+public void setName(String name) {
+	System.out.println("Voter.setName()");
+	this.name = name;
+}
+
+public void setAge(float age) {
+	System.out.println("Voter.setAge()");
+	this.age = age;
+}
+
+public void setDov(Date dov) {
+	System.out.println("Voter.setDov()");
+	this.dov = dov;
+}
+public void myInit() {
+	System.out.println("Voter.myInit()");
+	dov=new Date();//initializing left over property
+	boolean flag=false;
+	if(name==null) {
+		System.out.println("Name must not be null");
+		flag=true;
+	}
+	if(age<0)
+		age=age*-1;
+	if(age>100) {
+		System.out.println("age must not>100");
+		flag=true;
+}
+	if(flag)
+		throw new IllegalArgumentException("invalid inputs");
+}
+
+//destory life cycle method
+public void myDestroy() {
+	System.out.println("Voter.myDestroy()");
+	//nulify bean properties
+	name=null;
+	age=0;
+	dov=null;
+}
+
+ //b.method
+public String checkVotingElgibility() {
+	System.out.println("Voter.checkVotingElgibility()");
+	//b.logic
+	if(age>=18)
+		return "mr/mrs/miss +name+ u are eligiblie for voting  ->"+dov;
+	else
+		return "mr/mrs/miss +name+ u are not eligiblie for voting  ->"+dov;
+		
+}//method
+   
+}//class
