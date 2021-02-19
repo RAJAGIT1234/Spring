5b584efb2159f000cdfd1ae1797e2f7188c603b5
RAJA <RAJA@192.168.100.6>
2021-02-19 16:17:23 -0500

    My First MVC APP

---
+++ MVCPro1-FirstApp-ShowingHomePage/src/main/java/com/nt/controller/ShowHome.java
@@ -0,0 +1,20 @@
+package com.nt.controller;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class ShowHome implements Controller {
+
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	   //b.logic or request processing logic
+		Date d=new Date();
+		//return MAV
+		return new ModelAndView("home","sysDate",d);
+	}
+
+}
