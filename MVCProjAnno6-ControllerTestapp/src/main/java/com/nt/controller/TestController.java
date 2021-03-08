d98a384d3d116c52b87ee6938dc90330d1dcdb2c
RAJA <RAJA@192.168.100.9>
2021-03-08 08:52:48 -0500

    MVC Layered App

--- MVCProjAnno6-ControllerTestapp/src/main/java/com/nt/controller/TestController.java
+++ MVCProjAnno6-ControllerTestapp/src/main/java/com/nt/controller/TestController.java
@@ -1,5 +1,9 @@
 package com.nt.controller;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,6 +11,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class TestController {
 
+	@Autowired
+	private ServletContext sc;
+	
+	@Autowired
+	private ServletConfig sg;
 	@RequestMapping("/welcome")
 	public String showHome() {
 		return "home";
@@ -18,6 +27,10 @@ public class TestController {
 	
 	@RequestMapping(value="/register",method=RequestMethod.POST)//Get
 	public String saveData2() {
+		
+		System.out.println("displaying Employee");
+		System.out.println("webapp name:"+sc.getContextPath());
+		System.out.println("webapp path:"+sc.getRealPath("/"));
 		return "result2";
 	}
 	
